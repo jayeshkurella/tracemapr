9dcbe2a6fb4858853a7e9db5f246564f32ff657b
sanketl0 <sanketlodhe666@gmail.com>
2025-08-22 15:27:35 +0530

    changes

--- New_Backend/settings.py
+++ New_Backend/settings.py
@@ -83,13 +83,14 @@ WSGI_APPLICATION = 'New_Backend.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#         'NAME': 'tracemapr',
+#         'NAME': 'New_Backend',
 #         'USER': 'postgres',
-#         'PASSWORD': 'Swagati@1311',
+#         'PASSWORD': 'Sanket@123',
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     }
 # }
+
 
 # beta (testing)
 DATABASES = {
@@ -178,7 +179,7 @@ if os.name == 'nt':
     os.environ['PATH'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
     os.environ['PROJ_LIB'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo\\data\\proj')
 
-# GDAL_LIBRARY_PATH = r'C:\Users\lenovo\Desktop\Coderize\Chhaya_Project\Chhayya_Backend\env\Lib\site-packages\osgeo\gdal.dll'
+# GDAL_LIBRARY_PATH = r'C:\Users\sanke\Desktop\coderize\TraceMapr\Chhaya_new_backend\env\Lib\site-packages\osgeo\gdal.dll'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
