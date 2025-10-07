1574b07c9902c75bb41752d4999336b602d5e619
Swagati13 <swagatikashid760@gmail.com>
2025-10-07 12:08:10 +0530

    new updated code

--- user_management/urls.py
+++ user_management/urls.py
@@ -4,10 +4,12 @@ from .views import CaseCountAPIView
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import FeatureViewSet, RoleFeatureAccessViewSet
+from .views import UserFeatureAccessViewSet
 
 router = DefaultRouter()
 router.register("features", FeatureViewSet, basename="features")
 router.register("role-feature-access", RoleFeatureAccessViewSet, basename="role-feature-access")
+router.register("user-feature-access", UserFeatureAccessViewSet, basename="user-feature-access")
 urlpatterns = [
     path('count/', UserCountAPIView.as_view(), name='user-count'),
     path('case-management/count/', CaseCountAPIView.as_view(), name='case-count'),
