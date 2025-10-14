22bd5346c8c0c0eeef9d15747dae2a252753fce6
sanketl0 <sanketlodhe666@gmail.com>
2025-10-14 16:31:03 +0530

    added new file

--- Mainapp/authentication/user_authentication.py
+++ Mainapp/authentication/user_authentication.py
@@ -297,8 +297,8 @@ class AuthAPIView(APIView):
         user.reset_token_created_at = timezone.now()
         user.save()
 
-        reset_url = f"https://beta.tracemapr.com/authentication/reset-password/{reset_token}"
-        # reset_url = f"https://tracemapr.com/authentication/reset-password/{reset_token}"
+        # reset_url = f"https://beta.tracemapr.com/authentication/reset-password/{reset_token}"
+        reset_url = f"https://tracemapr.com/authentication/reset-password/{reset_token}"
         # reset_url = f"https://http://127.0.0.1:8000/reset-password/{reset_token}"
         send_mail("Password Reset Request",
                   f"Click the link to reset your password: {reset_url}",
